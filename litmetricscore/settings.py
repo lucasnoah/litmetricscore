5ccc4e919b94c9d77613d081bff92c1ac0e1c6a5
Lucas Noah <lucas.bird.noah@gmail.com>
2016-04-25 21:35:00 -0700

    Update settings.py

--- litmetricscore/settings.py
+++ litmetricscore/settings.py
@@ -100,7 +100,6 @@ DATABASES = {
             'NAME': 'litmetrics',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             'USER': 'litmetrics',
-            'PASSWORD': 'litmetrics',
             'HOST': 'db',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             'PORT': '5432',                      # Set to empty string for default.
         },
