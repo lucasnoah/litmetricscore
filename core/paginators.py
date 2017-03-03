2e2e24beaf3c700ea5e98c09d11ed9b575e2ef63
lucas <lucas.bird.noah@gmail.com>
2017-03-03 15:23:57 -0800

    update token count

--- core/paginators.py
+++ core/paginators.py
@@ -1,6 +1,9 @@
 from rest_framework.pagination import PageNumberPagination
 
 class TokenPagination(PageNumberPagination):
+    """
+    add a comment
+    """
 
     page_size = 700
     page_size_query_param = 'page_size'
